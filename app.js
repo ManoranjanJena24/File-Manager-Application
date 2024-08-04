7194b4092d7af9ba08181403ef09443e4f82a4ca
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-08-04 10:23:26 +0530

    Started the server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
